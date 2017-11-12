fd24896f70c26dd1ddc8461f16deb0c3a2304a7e
Joel Wallis Jucá <joelwallis@gmail.com>
2017-11-12 14:12:35 -0300

    Adds HTTP server functionality

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
+from flask import Flask
 
 class Blockchain(object):
     def __init__(self):
@@ -97,3 +98,28 @@ class Blockchain(object):
     @property
     def last_block(self):
         return self.chain[-1]
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
