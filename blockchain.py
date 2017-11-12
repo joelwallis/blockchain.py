8460ce5e7250766c3243ee72cd2d24790bf0890a
Joel Wallis Jucá <joelwallis@gmail.com>
2017-11-12 12:26:32 -0300

    Implementations of Blockchain#proof_of_work() and Blockchain.valid_proof()

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
@@ -48,6 +49,23 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm
+        - Find a number p' such that (pp') contains leading 4 zeroes, where p is
+          the previous p'
+        - p is the previous proof, and p' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
     @staticmethod
     def hash(block):
         """
@@ -60,6 +78,21 @@ class Blockchain(object):
         # We must be sure the dictionary keys are ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: does hash(last_proof, proof) contain 4 leading zeroes?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False otherwise
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] === '0000'
+
 
     @property
     def last_block(self):
